0cadf966c251766dcb4ac44877149644dd53088d
Munyounghyun <fldh3369@naver.com>
2022-07-15 14:30:40 +0900

    lifecycle 확인

--- src/component/BoxClass.js
+++ src/component/BoxClass.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
 
 export default class BoxClass extends Component {
+  componentWillUnmount() {
+    console.log("바이바이");
+  }
   render() {
     return <div>Box{this.props.num}</div>;
   }
